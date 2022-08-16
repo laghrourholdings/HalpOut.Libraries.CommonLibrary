0c9bb4e25207e70ff9b6adafb852038a81ce0e63
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2022-08-16 14:49:00 -0400

    Update ServiceSettings.cs

--- src/CommonLibrary.AspNetCore/Settings/ServiceSettings.cs
+++ src/CommonLibrary.AspNetCore/Settings/ServiceSettings.cs
@@ -2,9 +2,13 @@
 
 namespace CommonLibrary.AspNetCore.Settings;
 
+ /// <summary>
+    /// ServiceSettings provides utility and additionnal services data.
+/// </summary>
 public class ServiceSettings
 {
     public string ServiceName { get; init; }
     public static string GetMessage(ref LoggingInterpolatedStringHandler handler) => handler.ToString();
+   
 
 }
