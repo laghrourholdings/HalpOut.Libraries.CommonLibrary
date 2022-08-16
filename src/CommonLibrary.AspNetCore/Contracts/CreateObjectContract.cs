265bab343e21dd4aa4c68dac807a3778960b1742
Mohamed Anis Laghrour <mohamed.laghrour@outlook.com>
2022-08-16 08:43:51 -0400

    Fix

--- src/CommonLibrary.AspNetCore/Contracts/CreateObjectContract.cs
+++ src/CommonLibrary.AspNetCore/Contracts/CreateObjectContract.cs
@@ -4,11 +4,11 @@ using CommonLibrary.Core;
 
 namespace CommonLibrary.AspNetCore.Contracts;
 
+//Gateway to Internal
+public record CreateObject(IServiceBusMessage Payload);
 //Internal to Log
-
 public record LogCreateObject(ServiceBusLogContext<ServiceBusRequest<IIObject>> Payload);
-
-
 //Log to Internal
 public record LogCreateObjectResponse(ServiceBusMessageReponse<IIObject> Payload);
-
+//Internal to Gateway
+public record CreateObjectResponse(IServiceBusRequest<IIObject> Payload);
--- src/CommonLibrary.AspNetCore/Contracts/Gateway_Internal_Services/Contracts.cs
+++ src/CommonLibrary.AspNetCore/Contracts/Gateway_Internal_Services/Contracts.cs
@@ -6,11 +6,9 @@ namespace CommonLibrary.AspNetCore.Contracts;
 
 //Gateway to Internal
 public record RegisterObject(IObjectServiceBusRequest<IIObject> Payload);
-public record CreateObject(IServiceBusMessage Payload);
 public record GetAllObjects(IServiceBusRequest<IIObject> Payload);
 
 
 //Internal to Gateway
-public record CreateObjectResponse(IServiceBusRequest<IIObject> Payload);
 public record RegisterObjectResponse(IServiceBusMessageResponse<Guid> Payload);
 public record GetAllObjectsResponse(IObjectServiceBusMessageResponse<IIObject> Payload);
