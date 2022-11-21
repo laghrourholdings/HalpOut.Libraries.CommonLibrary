4e9e183f4f481c57b29e39e86412c60862dfa550
Mohamed Anis Laghrour <mohamed.laghrour@outlook.com>
2022-11-21 15:23:16 -0500

    fix radar get loghandles

---
+++ src/CommonLibrary.ClientServices/Logging/Implementations/LogHandleRepository.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq.Expressions;
+using CommonLibrary.Core;
+using CommonLibrary.Logging;
+using CommonLibrary.Settings;
+using Flurl;
+using Flurl.Http;
+
+namespace CommonLibrary.ClientServices.Logging.Implementations;
+
+public class LogHandleRepository : IRepository<LogHandle>
+{
+    public async Task<IEnumerable<LogHandle>?> GetAllAsync()
+    {
+        return await ServicesSettings.GatewayServiceDevURL
+            .AppendPathSegment("admin/logs")
+            .GetJsonAsync<IEnumerable<LogHandle>>();
+    }
+
+    public Task<IEnumerable<LogHandle>?> GetAllAsync(
+        Expression<Func<LogHandle, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LogHandle?> GetAsync(
+        Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LogHandle?> GetAsync(
+        Expression<Func<LogHandle, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CreateAsync(
+        LogHandle entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RangeAsync(
+        IEnumerable<LogHandle> entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(
+        LogHandle entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateOrCreateAsync(
+        LogHandle entity)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/CommonLibrary/Settings/ServicesSettings.cs
+++ src/CommonLibrary/Settings/ServicesSettings.cs
@@ -4,4 +4,5 @@ public static class ServicesSettings
 {
     public static readonly string? GatewayServiceDevURL = "https://localhost:4044/api/v1";
     public static readonly string? InternalServiceDevURL = "https://localhost:4042/api/v1";
+    public static readonly string? LogServiceDevURL = "https://localhost:4041/api/v1";
 }
