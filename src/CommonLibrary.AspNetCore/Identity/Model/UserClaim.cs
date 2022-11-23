de6d936728da8a8dd9d199989b46a3e3d936f4af
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2022-11-23 09:14:10 -0500

    Not yet working AuthSessions

--- src/CommonLibrary.AspNetCore/Identity/Model/UserClaim.cs
+++ src/CommonLibrary.AspNetCore/Identity/Model/UserClaim.cs
@@ -5,5 +5,6 @@ public static class UserClaimTypes
     public static string UserDetailsId = "UserDetailsId";
     public static string UserDeviceId = "UserDeviceId";
     public static string UserInterestId = "UserInterestId";
+    public static string UserSessionId = "UserSessionId";
     public static string LogHandleId = "LogHandleId";
 }
