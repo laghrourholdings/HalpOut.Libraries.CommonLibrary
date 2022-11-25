f052d08cd9b9bc312ddb1cf20a5434af0b9b76b7
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2022-11-25 11:55:18 -0500

    Refactor User

---
+++ src/CommonLibrary.AspNetCore/Core/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using CommonLibrary.Core;
+using Microsoft.AspNetCore.Identity;
+
+namespace CommonLibrary.AspNetCore;
+
+public interface IUserRepository<T> : IRepository<T>
+{
+    public Task SignOutAsync(T user);
+    public Task SignInAsync(T user, bool isPersistent = false);
+}
--- src/CommonLibrary.AspNetCore/Identity/Model/UserSession.cs
+++ src/CommonLibrary.AspNetCore/Identity/Model/UserSession.cs
@@ -2,14 +2,17 @@
 
 namespace CommonLibrary.AspNetCore.Identity.Model;
 
-public class UserSession : IObject
+public class UserSession : IObject, ISuspendable
 {
     public Guid Id { get; set; }
-    public Guid UserId { get; set; }
-    public Guid? DeviceId { get; set; }
+    public UserDevice DeviceId { get; set; }
     public string Key { get; set; }
     public DateTimeOffset CreationDate { get; set; }
     public DateTimeOffset? ExpirationDate { get; set; }
     public string? Descriptor { get; set; }
     public byte[] RawAuthenticationTicket { get; set; }
+
+    public bool IsSuspended { get; set; } = false;
+    public DateTimeOffset SuspendedDate { get; set; }
+    public Guid SuspendedBy { get; set; } = Guid.Empty;
 }
