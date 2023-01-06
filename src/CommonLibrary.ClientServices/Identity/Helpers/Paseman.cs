21102513b5b2d8a60582cf11e13ccf47969a07fd
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2023-01-06 05:27:58 -0500

    PSec helper for PASETO auth Not yet working.

---
+++ src/CommonLibrary.ClientServices/Identity/Helpers/Paseman.cs
@@ -0,0 +1,24 @@
+﻿using Paseto;
+using Paseto.Builder;
+
+namespace CommonLibrary.ClientServices.Identity.Helpers;
+
+public static class Paseman
+{
+
+    public static PasetoTokenValidationResult VerifyToken(
+        string token,
+        byte[] publicKey,
+        PasetoTokenValidationParameters? parameters = null, ProtocolVersion version = ProtocolVersion.V4)
+    {
+        var builder = new PasetoBuilder()
+            .Use(version, Purpose.Public)
+            .WithPublicKey(publicKey);
+        PasetoTokenValidationResult result;
+        if (parameters != null)
+            result = builder.Decode(token, parameters);
+        else
+            result = builder.Decode(token);
+        return result;
+    }
+}
