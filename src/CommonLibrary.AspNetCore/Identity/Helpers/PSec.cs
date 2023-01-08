33512f707874c9a4992bea3ebc66848df24ad267
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2023-01-08 02:54:46 -0500

    Token working for basic auth

--- src/CommonLibrary.AspNetCore/Identity/Helpers/PSec.cs
+++ src/CommonLibrary.AspNetCore/Identity/Helpers/PSec.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 using NaCl.Core.Internal;
 using Paseto;
 using Paseto.Builder;
@@ -10,7 +11,8 @@ public static class PSec
 {
     public static readonly byte[] DebugSymmetryKey 
         = CryptoBytes.FromHexString("707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f");
-    public class TokenSignature
+    
+    /*public class TokenSignature
     {
         public PasetoTokenValidationResult Result { get; }
 
@@ -26,14 +28,24 @@ public static class PSec
         }
         public byte[] PublicKey { get; }
         public Guid SessionId { get;  } 
-    }
+    }*/
     
     public static PasetoAsymmetricKeyPair GenerateAsymmetricKeyPair(ProtocolVersion version = ProtocolVersion.V4)
     {
         return new PasetoBuilder().Use(version, Purpose.Public).GenerateAsymmetricKeyPair(RandomNumberGenerator.GetBytes(32));
     }
     
+    public static string EncodeTo64(string toEncode)
+    {
+        byte[] toEncodeAsBytes = Encoding.ASCII.GetBytes(toEncode);
+        return Convert.ToBase64String(toEncodeAsBytes);
+    }
 
+    public static string DecodeFrom64(string encodedData)
+    {
+        byte[] encodedDataAsBytes = Convert.FromBase64String(encodedData);
+        return Encoding.ASCII.GetString(encodedDataAsBytes);
+    }
     public static PasetoBuilder CreateTokenPipe(
         string issuer,
         string audience,
@@ -52,18 +64,17 @@ public static class PSec
         byte[] publicKey,
         PasetoTokenValidationParameters? parameters = null, ProtocolVersion version = ProtocolVersion.V4)
     {
-        var builder = new PasetoBuilder()
+        
+        
+        var result = new PasetoBuilder()
             .Use(version, Purpose.Public)
-            .WithPublicKey(publicKey);
-        PasetoTokenValidationResult result;
-        if (parameters != null)
-            result = builder.Decode(token, parameters);
-        else
-            result = builder.Decode(token);
+            .WithPublicKey(publicKey)
+            .Decode(token, parameters);
+       
         return result;
     }
     
-    public static TokenSignature VerifyTokenSignature(
+    /*public static PasetoTokenValidationResult VerifyTokenSignature(
         string token,
         byte[] symmetricKey,
         PasetoTokenValidationParameters? parameters = null, ProtocolVersion version = ProtocolVersion.V4)
@@ -75,128 +86,53 @@ public static class PSec
             .WithSharedKey(symmetricKey)
             .Decode(footer);
         if (!footerDecryptResult.IsValid)
-            return new TokenSignature(footerDecryptResult);
+            return new PasetoTokenValidationResult(footerDecryptResult);
         var footerClaims = footerDecryptResult.Paseto.Payload.ToDictionary(x => x.Key, x => x.Value);
         var pk = CryptoBytes.FromHexString(footerClaims["pk"].ToString());
         var tokenVerifResult = VerifyToken(
             token,
             pk,
             parameters);
-        if (!tokenVerifResult.IsValid)
-            return new TokenSignature(tokenVerifResult);
-        return new TokenSignature(tokenVerifResult, pk, Guid.Parse(footerClaims["sid"].ToString()));
-    }
+        return tokenVerifResult;
+    }*/
 
     public static string Sign(
         this PasetoBuilder _builder,
-        byte[] publicKey,
         byte[] secretKey,
-        byte[] symmetricKey,
-        string sessionId,
-        DateTime sessionExpiration,
-    ProtocolVersion version = ProtocolVersion.V4)
-    {
-        // Convert.ToBase64String(
-        //     Encoding.UTF8.GetBytes(
-        //         new PasetoBuilder()
-        //             .Expiration(sessionExpiration)
-        //             //Encoding.UTF8.GetString(publicKey)
-        //             .AddClaim("pk",CryptoBytes.ToHexStringLower(publicKey))
-        //             .AddClaim("sid",sessionId)
-        //             .Use(version, Purpose.Local)
-        //             .WithSharedKey(symmetricKey)
-        //             .Encode())
-        // ))
-        return _builder
-            .AddFooter(
-                new PasetoBuilder()
-                    .Expiration(sessionExpiration)
-                    //Encoding.UTF8.GetString(publicKey)
-                    .AddClaim("pk",CryptoBytes.ToHexStringLower(publicKey))
-                    .AddClaim("sid",sessionId)
-                    .Use(version, Purpose.Local)
-                    .WithSharedKey(symmetricKey)
-                    .Encode())
+        ProtocolVersion version = ProtocolVersion.V4) =>
+        _builder
             .Use(version, Purpose.Public)
             .WithSecretKey(secretKey)
             .Encode();
-    }
-    //TODO: DecodePayload and DecodeFooter require a WithKey() function call.. find way to make that work.
-    //Replace footer with regular AES encryption without Paseto token.
-    public static string DecodePayload(string token, ProtocolVersion version = ProtocolVersion.V4)
-    {
-        return new PasetoBuilder()
+
+    /*public static string Encrypt(this PasetoBuilder _builder, 
+        byte[] symmetricKey, 
+        ProtocolVersion version = ProtocolVersion.V4) =>
+            _builder.Use(version, Purpose.Local)
+            .WithSharedKey(symmetricKey)
+            .Encode();
+    
+    public static PasetoTokenValidationResult Decrypt(
+        this PasetoBuilder _builder,
+        string token,
+        byte[] symmetricKey,
+        PasetoTokenValidationParameters? parameters = null,
+        ProtocolVersion version = ProtocolVersion.V4) =>
+            _builder.Use(version, Purpose.Local)
+            .WithSharedKey(symmetricKey)
+            .Decode(token, parameters);*/
+    
+    
+    public static string DecodePayload(string token, ProtocolVersion version = ProtocolVersion.V4) =>
+        new PasetoBuilder()
             .Use(version, Purpose.Public)
             .DecodePayload(token);
-    }
-    
-    public static string DecodeFooter(string token, ProtocolVersion version = ProtocolVersion.V4)
-    {
-        return new PasetoBuilder()
-            .Use(version, Purpose.Public)
+
+    public static string DecodeFooter(string token, ProtocolVersion version = ProtocolVersion.V4) =>
+        new PasetoBuilder()
             .DecodeFooter(token);
-    }
-    
-    public static string DecodeHeader(string token, ProtocolVersion version = ProtocolVersion.V4)
-    {
-        return new PasetoBuilder()
-            .Use(version, Purpose.Public)
+
+    public static string DecodeHeader(string token, ProtocolVersion version = ProtocolVersion.V4) =>
+        new PasetoBuilder()
             .DecodeHeader(token);
-    }
 }
-
-
-/*
- var session = authDbContext.UserSessions.Include(x=>x.Device).SingleOrDefault(x=>x.CacheKey == key);
-                        if (session != null)
-                        {
- var rsa = RSA.Create();
-Console.WriteLine(ASCIIEncoding.UTF8.GetString(rsa.ExportRSAPrivateKey()));
-Console.WriteLine("PUBLIC:");
-Console.WriteLine(ASCIIEncoding.UTF8.GetString(rsa.ExportRSAPublicKey()));
-Console.WriteLine("PUBLIC:");
-var token = JwtBuilder.Create()
-    .WithAlgorithm(new RS256Algorithm(rsa,rsa))
-    .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
-    .AddClaim("Session",session.Id.ToString())
-    .AddClaims(ticket.Principal.Claims.Select(x=>new KeyValuePair<string, object>(x.Type, x.Value)))
-    .WithSecret("secret")
-    .Encode();
-httpContext.Response.Cookies.Append("Identity.Token",
-    token, new CookieOptions
-    {
-        Expires = DateTimeOffset.UtcNow.AddHours(1),
-        HttpOnly = true
-    });
-session.Token = token;
-session.PrivateKey = rsa.ExportRSAPrivateKey();
-session.PublicKey = rsa.ExportRSAPublicKey();
-
-var rsa2 = RSA.Create();
-//rsa2.ImportRSAPrivateKey(rsa.ExportRSAPrivateKey(), out _);
-//rsa2.ImportRSAPublicKey(rsa.ExportRSAPublicKey(), out _);
-try
-{
-    IJsonSerializer serializer = new JsonNetSerializer();
-    IDateTimeProvider provider = new UtcDateTimeProvider();
-    IJwtValidator validator = new JwtValidator(serializer, provider);
-    IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
-    IJwtAlgorithm algorithm = new RS256Algorithm(rsa2);
-    IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
-
-    var json = decoder.Decode(token);
-    Console.WriteLine(json);
-}
-catch (TokenNotYetValidException)
-{
-    Console.WriteLine("Token is not valid yet");
-}
-catch (TokenExpiredException)
-{
-    Console.WriteLine("Token has expired");
-}
-catch (SignatureVerificationException)
-{
-    Console.WriteLine("Token has invalid signature");
-}
-}*/
---
+++ src/CommonLibrary.AspNetCore/Identity/Helpers/Securoman.cs
@@ -0,0 +1,167 @@
+﻿using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Paseto;
+using Paseto.Cryptography.Key;
+
+namespace CommonLibrary.AspNetCore.Identity.Helpers;
+
+public static class Securoman
+{
+    public static PasetoTokenValidationParameters DefaultParameters { get; } = new PasetoTokenValidationParameters
+    {
+        ValidateLifetime = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidAudience = "auth.laghrour.com",
+        ValidIssuer = "laghrour.com"
+    };
+    private static byte[] AES_Encrypt(
+        byte[] plain,
+        byte[] sharedKey,
+        byte[] salt)
+    {
+        if (plain == null || plain.Length <= 0)
+            throw new ArgumentNullException("plain");
+        if (sharedKey == null || sharedKey.Length <= 0)
+            throw new ArgumentNullException("sharedKey");
+        if (salt == null || salt.Length <= 0)
+            throw new ArgumentNullException("salt");
+        
+        byte[] encryptedBytes;
+        
+        
+        using (MemoryStream ms = new MemoryStream())
+        {
+            using (RijndaelManaged AES = new RijndaelManaged())
+            {
+                AES.KeySize = 256;
+                AES.BlockSize = 128;
+
+                var key = new Rfc2898DeriveBytes(sharedKey, salt, 1000);
+                AES.Key = key.GetBytes(AES.KeySize / 8);
+                AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                AES.Mode = CipherMode.CBC;
+
+                using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(plain, 0, plain.Length);
+                    cs.Close();
+                }
+                encryptedBytes = ms.ToArray();
+            }
+        }
+
+        return encryptedBytes;
+    }
+    private static byte[] AES_Decrypt(
+        byte[] cipher,
+        byte[] sharedKey,
+        byte[] salt)
+    {
+        if (cipher == null || cipher.Length <= 0)
+            throw new ArgumentNullException("bytesToBeEncrypted");
+        if (sharedKey == null || sharedKey.Length <= 0)
+            throw new ArgumentNullException("sharedKey");
+        if (salt == null || salt.Length <= 0)
+            throw new ArgumentNullException("salt");
+        byte[] decryptedBytes;
+
+        using (MemoryStream ms = new MemoryStream())
+        {
+            using (RijndaelManaged AES = new RijndaelManaged())
+            {
+                AES.KeySize = 256;
+                AES.BlockSize = 128;
+
+                var key = new Rfc2898DeriveBytes(sharedKey, salt, 1000);
+                AES.Key = key.GetBytes(AES.KeySize / 8);
+                AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                AES.Mode = CipherMode.CBC;
+
+                using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(cipher, 0, cipher.Length);
+                    cs.Close();
+                }
+                decryptedBytes = ms.ToArray();
+            }
+        }
+
+        return decryptedBytes;
+    }
+    
+    private static string EncryptPublicKey(byte[] decryptedPublicKey, byte[] symmetricKey, string? sessionId = null)
+    {
+        // Get the bytes of the string
+        byte[] IV;
+        IV = sessionId is null ? new byte[16] { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 2, 3 } : Encoding.UTF8.GetBytes(sessionId);
+
+        // Hash the password with SHA256
+        var sharedKey = SHA256.Create().ComputeHash(symmetricKey);
+
+        byte[] bytesEncrypted = AES_Encrypt(decryptedPublicKey, sharedKey, IV);
+        
+        
+        return Convert.ToHexString(bytesEncrypted);
+    }
+    
+    private static byte[] DecryptPublicKey(byte[] encryptedPublicKey, byte[] symmetricKey, string? sessionId = null)
+    {
+        // Get the bytes of the string
+        byte[] IV;
+        IV = sessionId is null ? new byte[16] { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 2, 3 } : Encoding.UTF8.GetBytes(sessionId);
+
+        // Hash the password with SHA256
+        var sharedKey = SHA256.Create().ComputeHash(symmetricKey);
+
+        byte[] bytesDecrypted = AES_Decrypt(encryptedPublicKey, sharedKey, IV);
+        
+        return bytesDecrypted;
+    }
+    
+    public static string GenerateToken(
+        PasetoAsymmetricKeyPair keyPair,
+        IEnumerable<Claim> claims,string sessionId)
+    {
+        var publicKey = keyPair.PublicKey.Key.ToArray();
+        var secretKey = keyPair.SecretKey.Key.ToArray();
+        
+        //TODO refactor to not have hardcoded stuff
+        var tokenBuilder = PSec.CreateTokenPipe("auth.laghrour.com","laghrour.com",DateTime.UtcNow.AddMinutes(5));
+        tokenBuilder.AddClaim("sid", sessionId);
+        tokenBuilder.AddFooter(EncryptPublicKey(publicKey, PSec.DebugSymmetryKey));
+        foreach (var claim in claims)
+        {
+            tokenBuilder.AddClaim(claim.Type, claim.Value);
+        }
+        
+        return tokenBuilder.Sign(secretKey);
+    }
+
+    public static TokenSignature VerifyToken(string token)
+    {
+        var footer = PSec.DecodeFooter(token);
+        var publicKey = DecryptPublicKey(Convert.FromHexString(footer), PSec.DebugSymmetryKey);
+        
+        return new TokenSignature(PSec.VerifyToken(token, publicKey, DefaultParameters), publicKey);
+    }
+
+    public class TokenSignature
+    {
+        public PasetoTokenValidationResult Result { get; }
+
+        public TokenSignature(PasetoTokenValidationResult result, byte[] publicKey, Guid sessionId = default)
+        {
+            Result = result;
+            PublicKey = publicKey;
+        }
+        public TokenSignature(PasetoTokenValidationResult result)
+        {
+            Result = result;
+        }
+        public byte[] PublicKey { get; }
+    }
+}
