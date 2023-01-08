638593cd309098818da943815f161050f74d2ad5
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2023-01-08 02:54:46 -0500

    Token working for basic auth

--- src/CommonLibrary.AspNetCore/Identity/Helpers/PSec.cs
+++ src/CommonLibrary.AspNetCore/Identity/Helpers/PSec.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 using NaCl.Core.Internal;
 using Paseto;
 using Paseto.Builder;
@@ -10,7 +11,8 @@ public static class PSec
 {
     public static readonly byte[] DebugSymmetryKey 
         = CryptoBytes.FromHexString("707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f");
-    public class TokenSignature
+    
+    /*public class TokenSignature
     {
         public PasetoTokenValidationResult Result { get; }
 
@@ -26,14 +28,24 @@ public static class PSec
         }
         public byte[] PublicKey { get; }
         public Guid SessionId { get;  } 
-    }
+    }*/
     
     public static PasetoAsymmetricKeyPair GenerateAsymmetricKeyPair(ProtocolVersion version = ProtocolVersion.V4)
     {
         return new PasetoBuilder().Use(version, Purpose.Public).GenerateAsymmetricKeyPair(RandomNumberGenerator.GetBytes(32));
     }
     
+    public static string EncodeTo64(string toEncode)
+    {
+        byte[] toEncodeAsBytes = Encoding.ASCII.GetBytes(toEncode);
+        return Convert.ToBase64String(toEncodeAsBytes);
+    }
 
+    public static string DecodeFrom64(string encodedData)
+    {
+        byte[] encodedDataAsBytes = Convert.FromBase64String(encodedData);
+        return Encoding.ASCII.GetString(encodedDataAsBytes);
+    }
     public static PasetoBuilder CreateTokenPipe(
         string issuer,
         string audience,
@@ -52,18 +64,17 @@ public static class PSec
         byte[] publicKey,
         PasetoTokenValidationParameters? parameters = null, ProtocolVersion version = ProtocolVersion.V4)
     {
-        var builder = new PasetoBuilder()
+        
+        
+        var result = new PasetoBuilder()
             .Use(version, Purpose.Public)
-            .WithPublicKey(publicKey);
-        PasetoTokenValidationResult result;
-        if (parameters != null)
-            result = builder.Decode(token, parameters);
-        else
-            result = builder.Decode(token);
+            .WithPublicKey(publicKey)
+            .Decode(token, parameters);
+       
         return result;
     }
     
-    public static TokenSignature VerifyTokenSignature(
+    /*public static PasetoTokenValidationResult VerifyTokenSignature(
         string token,
         byte[] symmetricKey,
         PasetoTokenValidationParameters? parameters = null, ProtocolVersion version = ProtocolVersion.V4)
@@ -75,74 +86,55 @@ public static class PSec
             .WithSharedKey(symmetricKey)
             .Decode(footer);
         if (!footerDecryptResult.IsValid)
-            return new TokenSignature(footerDecryptResult);
+            return new PasetoTokenValidationResult(footerDecryptResult);
         var footerClaims = footerDecryptResult.Paseto.Payload.ToDictionary(x => x.Key, x => x.Value);
         var pk = CryptoBytes.FromHexString(footerClaims["pk"].ToString());
         var tokenVerifResult = VerifyToken(
             token,
             pk,
             parameters);
-        if (!tokenVerifResult.IsValid)
-            return new TokenSignature(tokenVerifResult);
-        return new TokenSignature(tokenVerifResult, pk, Guid.Parse(footerClaims["sid"].ToString()));
-    }
+        return tokenVerifResult;
+    }*/
 
     public static string Sign(
         this PasetoBuilder _builder,
-        byte[] publicKey,
         byte[] secretKey,
-        byte[] symmetricKey,
-        string sessionId,
-        DateTime sessionExpiration,
-    ProtocolVersion version = ProtocolVersion.V4)
-    {
-        // Convert.ToBase64String(
-        //     Encoding.UTF8.GetBytes(
-        //         new PasetoBuilder()
-        //             .Expiration(sessionExpiration)
-        //             //Encoding.UTF8.GetString(publicKey)
-        //             .AddClaim("pk",CryptoBytes.ToHexStringLower(publicKey))
-        //             .AddClaim("sid",sessionId)
-        //             .Use(version, Purpose.Local)
-        //             .WithSharedKey(symmetricKey)
-        //             .Encode())
-        // ))
-        return _builder
-            .AddFooter(
-                new PasetoBuilder()
-                    .Expiration(sessionExpiration)
-                    //Encoding.UTF8.GetString(publicKey)
-                    .AddClaim("pk",CryptoBytes.ToHexStringLower(publicKey))
-                    .AddClaim("sid",sessionId)
-                    .Use(version, Purpose.Local)
-                    .WithSharedKey(symmetricKey)
-                    .Encode())
+        ProtocolVersion version = ProtocolVersion.V4) =>
+        _builder
             .Use(version, Purpose.Public)
             .WithSecretKey(secretKey)
             .Encode();
-    }
-    //TODO: DecodePayload and DecodeFooter require a WithKey() function call.. find way to make that work.
-    //Replace footer with regular AES encryption without Paseto token.
-    public static string DecodePayload(string token, ProtocolVersion version = ProtocolVersion.V4)
-    {
-        return new PasetoBuilder()
+
+    /*public static string Encrypt(this PasetoBuilder _builder, 
+        byte[] symmetricKey, 
+        ProtocolVersion version = ProtocolVersion.V4) =>
+            _builder.Use(version, Purpose.Local)
+            .WithSharedKey(symmetricKey)
+            .Encode();
+    
+    public static PasetoTokenValidationResult Decrypt(
+        this PasetoBuilder _builder,
+        string token,
+        byte[] symmetricKey,
+        PasetoTokenValidationParameters? parameters = null,
+        ProtocolVersion version = ProtocolVersion.V4) =>
+            _builder.Use(version, Purpose.Local)
+            .WithSharedKey(symmetricKey)
+            .Decode(token, parameters);*/
+    
+    
+    public static string DecodePayload(string token, ProtocolVersion version = ProtocolVersion.V4) =>
+        new PasetoBuilder()
             .Use(version, Purpose.Public)
             .DecodePayload(token);
-    }
-    
-    public static string DecodeFooter(string token, ProtocolVersion version = ProtocolVersion.V4)
-    {
-        return new PasetoBuilder()
-            .Use(version, Purpose.Public)
+
+    public static string DecodeFooter(string token, ProtocolVersion version = ProtocolVersion.V4) =>
+        new PasetoBuilder()
             .DecodeFooter(token);
-    }
-    
-    public static string DecodeHeader(string token, ProtocolVersion version = ProtocolVersion.V4)
-    {
-        return new PasetoBuilder()
-            .Use(version, Purpose.Public)
+
+    public static string DecodeHeader(string token, ProtocolVersion version = ProtocolVersion.V4) =>
+        new PasetoBuilder()
             .DecodeHeader(token);
-    }
 }
 
 
